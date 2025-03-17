de971d5075e842735dd1c897219b0f86519008e6
Buzayehu Chiko <buzayehugirma12@gmail.com>
2025-03-17 13:31:24 -0400

    Added EmployeeForm1 component

---
+++ src/components/EmployeeForm.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class EmployeeForm extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2>Employee Form</h2>
+      </div>
+    );
+  }
+}
+
+export default EmployeeForm;
+
